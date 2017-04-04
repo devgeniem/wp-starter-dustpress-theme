ce0bf04c0efb77a48d9959cb5054d96853a411ec
Ville Pietarinen <ville@geniem.com>
2017-04-04 13:38:15 +0300

    replaceable text-domain string

--- lib/setup.php
+++ lib/setup.php
@@ -15,14 +15,14 @@ if ( ! defined( 'ASSET_PATH' ) ) {
  */
 function setup() {
     // Make theme available for translation
-    \load_theme_textdomain( 'themename', get_template_directory() . '/lang' );
+    \load_theme_textdomain( 'themename-textdomain', get_template_directory() . '/lang' );
     // Enable plugins to manage the document title
     // http://codex.wordpress.org/Function_Reference/add_theme_support#Title_Tag
     \add_theme_support( 'title-tag' );
     // Register wp_nav_menu() menus
     // http://codex.wordpress.org/Function_Reference/register_nav_menus
     \register_nav_menus( [
-        'primary_navigation' => __( 'Primary Navigation', 'themename' ),
+        'primary_navigation' => __( 'Primary Navigation', 'themename-textdomain' ),
     ] );
     // Enable post thumbnails
     // http://codex.wordpress.org/Post_Thumbnails
