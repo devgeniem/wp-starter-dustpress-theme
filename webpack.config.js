d06ac80c2a69219cc12e328a278a4d931c2d43f8
Otto Rautamaa <otto.rautamaa@geniem.com>
2017-09-11 17:24:50 +0300

    Fix linter errors from webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 /* global __dirname process */
 
-const webpack                 = require('webpack');
-const ExtractTextPlugin       = require('extract-text-webpack-plugin');
-const autoprefixer            = require('autoprefixer');
-const autoprefixerBrowsers    = ['> 1%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1'];
+const webpack                 = require( 'webpack' );
+const ExtractTextPlugin       = require( 'extract-text-webpack-plugin' );
+const autoprefixer            = require( 'autoprefixer' );
+const autoprefixerBrowsers    = [ '> 1%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1' ];
 const path                    = require( 'path' ); // This resolves into the absolute path of the theme root.
 const env                     = process.env.NODE_ENV;
 
@@ -17,18 +17,22 @@ let config = {
         filename: '[name].js'
     },
     externals: {
+
         // Set jQuery to be an external resource.
         'jquery': 'jQuery'
     },
-    postcss: function () {
+    postcss: function() {
+
         // Enable autoprefixing.
-        return [autoprefixer({ browsers: autoprefixerBrowsers })];
+        return [ autoprefixer({ browsers: autoprefixerBrowsers }) ];
     },
     plugins: [
+
         // Extract all css into one file.
-        new ExtractTextPlugin('[name].css', {
+        new ExtractTextPlugin( '[name].css', {
             allChunks: true
         }),
+
         // Provide jQuery instance for all modules.
         new webpack.ProvidePlugin({
             jQuery: 'jquery'
@@ -39,29 +43,34 @@ let config = {
             {
                 test: /\.js$/,
                 loader: 'babel-loader',
+
                 // List paths to packages using ES6 to enable Babel compiling.
                 include: [
-                    path.resolve(__dirname, 'assets/scripts'),
-                    path.resolve(__dirname, 'node_modules/foundation-sites')
+                    path.resolve( __dirname, 'assets/scripts' ),
+                    path.resolve( __dirname, 'node_modules/foundation-sites' )
                 ],
                 query: {
+
                     // Do not use the .babelrc configuration file.
                     babelrc: false,
+
                     // The loader will cache the results of the loader in node_modules/.cache/babel-loader.
                     cacheDirectory: true,
+
                     // The 'transform-runtime' plugin tells babel to require the runtime instead of inlining it.
-                    plugins: ['transform-runtime'],
+                    plugins: [ 'transform-runtime' ],
+
                     // List enabled ECMAScript feature sets.
-                    presets: ['es2015', 'stage-0']
-                },
+                    presets: [ 'es2015', 'stage-0' ]
+                }
             },
             {
                 test: /\.css$/,
-                loader: ExtractTextPlugin.extract('style', 'css')
+                loader: ExtractTextPlugin.extract( 'style', 'css' )
             },
             {
                 test: /\.scss$/,
-                loader: ExtractTextPlugin.extract('style-loader', 'css!postcss!sass')
+                loader: ExtractTextPlugin.extract( 'style-loader', 'css!postcss!sass' )
             },
             {
                 test: /\.(woff(2)?|eot|ttf|otf)(\?[a-z0-9=\.]+)?$/,
@@ -78,8 +87,9 @@ let config = {
     }
 };
 
-if (env === 'production') {
+if ( env === 'production' ) {
     config.plugins.push(
+
         // Minify for the production environment.
         new webpack.optimize.UglifyJsPlugin({
             mangle: false,
