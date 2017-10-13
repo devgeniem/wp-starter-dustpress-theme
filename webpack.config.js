2149125b99c63da49afef14359887a830f69022e
Ville Siltala <ville.siltala@geniem.com>
2017-10-13 17:37:11 +0300

    Migrating to webpack 3 in progress..

--- webpack.config.js
+++ webpack.config.js
@@ -21,64 +21,81 @@ let config = {
         // Set jQuery to be an external resource.
         'jquery': 'jQuery'
     },
-    postcss: function() {
-
-        // Enable autoprefixing.
-        return [ autoprefixer({ browsers: autoprefixerBrowsers }) ];
-    },
+    // postcss: function() {
+    //
+    //     // Enable autoprefixing.
+    //     return [ autoprefixer({ browsers: autoprefixerBrowsers }) ];
+    // },
     plugins: [
 
         // Extract all css into one file.
         new ExtractTextPlugin( '[name].css', {
-            allChunks: true
+            allChunks: true,
+            disable: false
         }),
 
         // Provide jQuery instance for all modules.
         new webpack.ProvidePlugin({
             jQuery: 'jquery'
+        }),
+
+        new webpack.LoaderOptionsPlugin({
+            // test: /\.xxx$/, // may apply this only for some modules
+            options: {
+                postcss: function() {
+                    // Enable autoprefixing.
+                    return [ autoprefixer({ browsers: autoprefixerBrowsers }) ];
+                }
+            }
         })
+
     ],
     module: {
-        loaders: [
+        rules: [
             {
                 test: /\.js$/,
-                loader: 'babel-loader',
-
                 // List paths to packages using ES6 to enable Babel compiling.
                 include: [
                     path.resolve( __dirname, 'assets/scripts' ),
                     path.resolve( __dirname, 'node_modules/foundation-sites' )
                 ],
-                query: {
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        // Do not use the .babelrc configuration file.
+                        babelrc: false,
 
-                    // Do not use the .babelrc configuration file.
-                    babelrc: false,
+                        // The loader will cache the results of the loader in node_modules/.cache/babel-loader.
+                        cacheDirectory: true,
 
-                    // The loader will cache the results of the loader in node_modules/.cache/babel-loader.
-                    cacheDirectory: true,
+                        // The 'transform-runtime' plugin tells babel to require the runtime instead of inlining it.
+                        plugins: [ 'transform-runtime' ],
 
-                    // The 'transform-runtime' plugin tells babel to require the runtime instead of inlining it.
-                    plugins: [ 'transform-runtime' ],
+                        // List enabled ECMAScript feature sets.
+                        presets: [ 'es2015', 'stage-0' ]
+                    }
 
-                    // List enabled ECMAScript feature sets.
-                    presets: [ 'es2015', 'stage-0' ]
                 }
             },
             {
                 test: /\.css$/,
-                loader: ExtractTextPlugin.extract( 'style', 'css' )
+                use: ExtractTextPlugin.extract( 'style', 'css' )
             },
             {
                 test: /\.scss$/,
-                loader: ExtractTextPlugin.extract( 'style-loader', 'css!postcss!sass' )
+                use: ExtractTextPlugin.extract( 'style-loader', 'css!postcss!sass' )
             },
             {
                 test: /\.(woff(2)?|eot|ttf|otf)(\?[a-z0-9=\.]+)?$/,
-                loader: 'file?name=../fonts/[name].[ext]'
+                use: {
+                    loader: 'file?name=../fonts/[name].[ext]'
+                }
             },
             {
                 test: /\.(svg|gif|png|jpeg|jpg)(\?[a-z0-9=\.]+)?$/,
-                loader: 'file?name=../images/[name].[ext]'
+                use: {
+                    loader: 'file?name=../images/[name].[ext]'
+                }
             }
         ]
     },
@@ -92,10 +109,22 @@ if ( env === 'production' ) {
 
         // Minify for the production environment.
         new webpack.optimize.UglifyJsPlugin({
-            mangle: false,
+            mangle: {
+                // Do not mangle class names.
+                keep_classnames: true,
+                // Do not mangle function names.
+                keep_fnames: true
+            },
+            sourceMap: true,
             compress: {
+                // Do not drop unreferenced functions and variables.
                 unused: false
-            }
+            },
+            parallel: {
+                cache: true,
+                workers: 4
+            },
+            warnings: false
         })
     );
 }
