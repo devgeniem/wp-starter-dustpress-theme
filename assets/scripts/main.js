b02b63e7e0c980b370bec7bbf10edcb1d0fed14c
Ville Siltala <ville.siltala@geniem.com>
2017-11-07 11:30:55 +0200

    Fix require structure to enable JS minifying. Update docs.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -6,35 +6,11 @@
 require( 'babel-polyfill' );
 require( __dirname + '/modernizr.js' );
 
-// Require fastclick for a faster mobile UX.
-const FastClick = require( 'fastclick' );
-FastClick.attach( document.body );
-
 // Uncomment to include Foundation scripts.
 // require( 'foundation-sites' );
 
-// Run the theme scripts.
-let Theme = require( __dirname + '/theme.js' );
-
 // Export the theme controller for global usage.
-window.Theme = Theme;
-
-// Add your global scripts here.
-let globalControllers = [
-    require( __dirname + '/common.js' )
-];
-
-// Add your template-specific scripts here.
-let templateControllers = [
-    require( __dirname + '/page-frontpage.js' )
-];
-
-// Pass the required scripts and construct the global ones first.
-Theme.setGlobalControllers( globalControllers );
-Theme.setTemplateControllers( templateControllers );
-
-// Run the init function for all scripts.
-Theme.init();
+window.Theme = require( __dirname + '/theme.js' );
 
 // Require main style file here for concatenation.
 require( __dirname + '/../styles/main.scss' );
